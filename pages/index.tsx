e587d87e334a0c66ed3dfda2bd227ce33f4a2038
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-11-08 11:57:34 +0800

    Initial implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,39 @@
 import type { NextPage } from "next";
 import { useState } from "react";
 import Modal from "../components/Modal";
+import OtpInput from "../components/OtpInput";
 
 const Home: NextPage = () => {
-  //add state and a toggle handler
-  const [open, setOpen] = useState(false);
-  const handleToggle = () => setOpen((prev) => !prev);
+  const [otp, setOtp] = useState("");
   return (
     <div className="container">
-      <h1 className="text-2xl">Hello world</h1>
-      {/* opens the modal */}
-      <button className="btn btn-primary" onClick={handleToggle}>
-        Hello
-      </button>
-      <Modal open={open} onClose={handleToggle}>
-        <h3 className="font-bold text-lg">
-          Congratulations random Internet user!
-        </h3>
-        <p className="py-4">
-          You havve been selected for a chance to get one year of subscription
-          to use Wikipedia for free!
-        </p>
-        <div className="modal-action">
-          {/* closes the modal */}
-          <button className="btn btn-primary" onClick={handleToggle}>
-            Yay!
-          </button>
-        </div>
-      </Modal>
+      <div className="h-screen grid place-content-center bg-base-100">
+        <form
+          className="card shadow-md bg-base-200"
+          onSubmit={async (e) => {
+            e.preventDefault();
+          }}
+        >
+          <div className="card-body items-stretch text-center">
+            <div className="my-2">
+              <h2 className="text-xl"> One-Time Password</h2>
+              <p className="text-sm text-base-content/80">
+                {" "}
+                Input the code sent at jm****e@gmail.com
+              </p>
+            </div>
+            <OtpInput
+              value={otp}
+              onChange={(val) => {
+                setOtp(val);
+              }}
+            />
+            <button className="btn btn-primary mt-2" type="submit">
+              Verify OTP
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
