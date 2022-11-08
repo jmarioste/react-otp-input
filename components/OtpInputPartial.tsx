6f84250a3abbeb9d0fc943f5094714f2a74fe665
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-11-08 13:54:37 +0800

    Partial initial implementation

---
+++ components/OtpInputPartial.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect } from "react";
+
+/**
+ * Let's borrow some props from HTML "input". More info below:
+ *
+ * [Pick Documentation](https://www.typescriptlang.org/docs/handbook/utility-types.html#picktype-keys)
+ *
+ * [How to extend HTML Elements](https://reacthustle.com/blog/how-to-extend-html-elements-in-react-typescript)
+ */
+type PartialInputProps = Pick<
+  React.ComponentPropsWithoutRef<"input">,
+  "className" | "style"
+>;
+
+type Props = {
+  /**
+   * full value of the otp input, up to {size} characters
+   */
+  value: string;
+  onChange(value: string): void;
+  /**
+   * Number of characters/input for this component
+   */
+  size?: number;
+  /**
+   * Validation pattern for each input.
+   * e.g: /[0-9]{1}/ for digits only or /[0-9a-zA-Z]{1}/ for alphanumeric
+   */
+  validationPattern?: RegExp;
+} & PartialInputProps;
+
+const OtpInput = (props: Props) => {
+  const {
+    //Set the default size to 6 characters
+    size = 6,
+    //Default validation is digits
+    validationPattern = /[0-9]{1}/,
+    value,
+    onChange,
+    className,
+    ...restProps
+  } = props;
+
+  // Create an array based on the size.
+  const arr = new Array(size).fill("-");
+  return (
+    <div className="flex gap-2">
+      {/* Map through the array and render input components */}
+      {arr.map((_, index) => {
+        return (
+          <input
+            key={index}
+            {...restProps}
+            /**
+             * Add some styling to the input using daisyUI + tailwind.
+             * Allows the user to override the classname for a different styling
+             */
+            className={
+              className || `input input-bordered px-0 text-center w-14 h-16`
+            }
+            type="text"
+            inputMode="numeric"
+            autoComplete="one-time-code"
+            pattern={validationPattern.source}
+            maxLength={6}
+            value={value.at(index) ?? ""}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default OtpInput;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,18 @@
 import type { NextPage } from "next";
 import { useState } from "react";
-import OtpInput from "../components/OtpInput";
+import OtpInput from "../components/OtpInputPartial";
 
 const Home: NextPage = () => {
   const [otp, setOtp] = useState("");
   return (
     <div className="container">
       <div className="h-screen grid place-content-center bg-base-100">
-        <form
-          className="card shadow-md bg-base-200"
-          onSubmit={async (e) => {
-            e.preventDefault();
+        <OtpInput
+          value={otp}
+          onChange={(val) => {
+            setOtp(val);
           }}
-        >
-          <div className="card-body items-stretch text-center">
-            <div className="my-2">
-              <h2 className="text-xl"> One-Time Password</h2>
-              <p className="text-sm text-base-content/80">
-                {" "}
-                Input the code sent at jm****e@gmail.com
-              </p>
-            </div>
-            <OtpInput
-              value={otp}
-              onChange={(val) => {
-                setOtp(val);
-              }}
-            />
-            <button className="btn btn-primary mt-2" type="submit">
-              Verify OTP
-            </button>
-          </div>
-        </form>
+        />
       </div>
     </div>
   );
