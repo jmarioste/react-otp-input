7a7f070c30aca94efd1c769b8846ba3570e18dbc
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-11-08 17:48:18 +0800

    Fix import

--- components/OtpForm.tsx
+++ components/OtpForm.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import OtpInput from "components/OtpInputPartial";
+import OtpInput from "components/OtpInput";
 import classNames from "classnames";
 
 const OtpForm = () => {
